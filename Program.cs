2bee23cba9c2ee98bd103e6dd82ccd04555a406d
jui-wen-chiang <raewen.juiwenchiang@gmail.com>
2025-07-25 11:37:53 -0400

    feat:init project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/* 
+    The program should be completed in a single file and only the .cs file must be submitted.
+*/
+namespace gbc_comp1202_asgmt2
+{
+
+    /* Game Class Developer - Anastasiia */
+    class Game
+    { 
+
+    }
+    /* File & Search Handler Class Developer - Howard */
+    class FileHandler
+    {
+
+    }
+    /*  Menu & Main Program Developer - JuiWen */
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!\n");
+        }
+    }
+}
