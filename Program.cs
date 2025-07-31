87b77400adde6da202f65460b2209d8f40f5229b
poweredbyhoward <128647465+poweredbyhoward@users.noreply.github.com>
2025-07-30 23:27:56 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 /* 
     The program should be completed in a single file and only the .cs file must be submitted.
@@ -176,15 +177,148 @@ namespace gbc_comp1202_asgmt2
         {
             return quantity > 0;
         }
-    }
 
+    }
 
     /* File & Search Handler Class Developer - Howard */
-    class FileHandler
+    public class FileHandler
     {
+        StreamReader? sr = null;
+        private const string FILENAME = "VideoGames.txt";
 
+        public StreamReader? LoadFile()
+        {
+            try
+            {
+                sr = new StreamReader(FILENAME);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return sr;
+        }
+
+        public bool AppendFile(Game game)
+        {
+            StreamWriter? sw = null;
+            bool isSuccess = false;
+            try
+            {
+                sw = new StreamWriter("VideoGames.txt", true);
+                // toFileString() returns the game object as a string
+                sw.WriteLine(game.ToFileString());
+                isSuccess = true;
+            }
+            finally
+            {
+                sw?.Close();
+            }
+            return isSuccess;
+        }
+        // Lookup by item number
+        public bool Lookup(string itemNumber)
+        {
+            bool isSuccess = false;
+            // File loaded?
+            if ((sr = LoadFile()) != null)
+            {
+                string? fileLine;
+                // Line present?
+                while ((fileLine = sr.ReadLine()) != null)
+                {
+                    // Matching ItemNum?
+                    Game game = Game.FromFileString(fileLine);
+                    if (game.GetItemNumber() == itemNumber)
+                    {
+                        // Pretty print
+                        Console.WriteLine(game.ToString());
+                        isSuccess = true;
+                        break;
+                    }
+                }
+                sr.Close();
+            }
+            return isSuccess;
+        }
+        // Overloaded Lookup by max price
+        public bool Lookup(decimal maxPrice)
+        {
+            bool isSuccess = false;
+            Game[] gamesList = [];
+            int count = 0;
+            if ((sr = LoadFile()) != null)
+            {
+                string? fileLine;
+                while ((fileLine = sr.ReadLine()) != null)
+                {
+                    Game game = Game.FromFileString(fileLine);
+                    if (game.GetPrice() <= maxPrice)
+                    {
+                        Array.Resize(ref gamesList, ++count);
+                        gamesList[count - 1] = game;
+                        isSuccess = true;
+                    }
+                }
+                Console.WriteLine("\n{0} listings found under {1:C}:", gamesList.Length, maxPrice);
+                foreach (Game game in gamesList)
+                {
+                    Console.WriteLine(game.ToString());
+                }
+                sr.Close();
+            }
+            return isSuccess;
+        }
+
+        public bool ViewStats()
+        {
+            int lineCount = 0;
+            string? fileLine;
+
+            if ((sr = LoadFile()) != null)
+            {
+                while (sr.ReadLine() != null) lineCount++;
+                sr.Close();
+            }
+            else return false;
+
+            Game[] gamesList = new Game[lineCount];
+
+            if ((sr = LoadFile()) != null)
+            {
+                int i = 0;
+                while ((fileLine = sr.ReadLine()) != null)
+                {
+                    gamesList[i++] = Game.FromFileString(fileLine);
+                }
+                sr.Close();
+            }
+            else return false;
+
+            int productsTotal = 0;
+            decimal valueTotal = 0m;
+            decimal meanPrice;
+            Game cheapestGame = gamesList[0];
+            Game priciestGame = gamesList[0];
+            decimal price;
+
+            foreach (var game in gamesList)
+            {
+                price = game.GetPrice();
+                productsTotal += game.GetQuantity();
+                valueTotal += game.GetQuantity() * price;
+
+                if (price < cheapestGame.GetPrice()) cheapestGame = game;
+                else if (price >= priciestGame.GetPrice()) priciestGame = game;
+            }
+            meanPrice = valueTotal / productsTotal;
+            Console.WriteLine("\nThe mean price of {0} stocked items valued at {1:C}: {2:C}", productsTotal, valueTotal, meanPrice);
+            Console.WriteLine("\nThe price range of our products is between {0:C} ~ {1:C}", cheapestGame.GetPrice(), priciestGame.GetPrice());
+            Console.WriteLine("\nOur cheapest game is:\n{0}", cheapestGame);
+            Console.WriteLine("\nAnd most expensive game is:\n{0}", priciestGame);
+            return true;
+        }
     }
-
 
     /*  Menu & Main Program Developer - JuiWen */
     class Program
@@ -213,6 +347,7 @@ namespace gbc_comp1202_asgmt2
 
             userInput = Console.ReadLine();
 
+            FileHandler file = new FileHandler();
 
             switch (userInput)
             {
@@ -224,9 +359,14 @@ namespace gbc_comp1202_asgmt2
                     break;
                 case "3":
                     Console.WriteLine(menuItem[2]);
+                    // Example usage:
+                    // file.Lookup("0299"); // Item num
+                    // file.Lookup(59.99m); // Max Price
                     break;
                 case "4":
                     Console.WriteLine(menuItem[3]);
+                    // Example usage:
+                    // file.ViewStats();
                     break;
                 case "5":
                     Console.WriteLine(menuItem[4]);
